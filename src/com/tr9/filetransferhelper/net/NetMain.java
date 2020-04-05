615f5c89ccd43027083b24b4a830395364693aae
tr9 <233691@qq.com>
2020-04-05 10:59:59 +0800

    add test code

--- src/com/tr9/filetransferhelper/net/NetMain.java
+++ src/com/tr9/filetransferhelper/net/NetMain.java
@@ -2,6 +2,7 @@ package com.tr9.filetransferhelper.net;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class NetMain implements Runnable {
 
@@ -13,7 +14,15 @@ public class NetMain implements Runnable {
     private void buildSocket(){
         try {
             ServerSocket serverSocket = new ServerSocket(8888);
-            serverSocket.accept();
+            Socket socket = serverSocket.accept();
+            String inetHostAddress = socket.getInetAddress().getHostAddress();
+            String inetHostName = socket.getInetAddress().getHostName();
+            String localHostAddress = socket.getLocalAddress().getHostAddress();
+            String localHostName = socket.getLocalAddress().getHostName();
+            System.out.println(inetHostAddress);
+            System.out.println(inetHostName);
+            System.out.println(localHostAddress);
+            System.out.println(localHostName);
         } catch (IOException e) {
             e.printStackTrace();
         }
